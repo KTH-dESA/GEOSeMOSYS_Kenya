d79b6c1ecdb44b3ab733aa1184e3ab5e247c19fc
Nandi Moksnes <nandi.moksnes@gmail.com>
2022-01-19 09:27:15 +0100

    gaussian mixture clustering

--- src/Results_extraction.py
+++ src/Results_extraction.py
@@ -1,6 +1,7 @@
 import geopandas as gpd
 import pandas as pd
 import numpy as np
+from sklearn.mixture import GaussianMixture
 from sklearn.cluster import KMeans
 
 
@@ -11,14 +12,14 @@ def read_data(data):
     :return:
     """
     col =['param','region','tech','f','2016','2017','2018','2019','2020','2021','2022','2023','2024','2025','2026','2027','2028','2029','2030','2031','2032','2033','2034','2035','2036','2037','2038','2039','2040','2041']
-    df = pd.read_csv(data, sep='\t', names=col)
+    df = pd.read_csv(data, sep='\s+', names=col)
     param = df.param.unique()
     dict_results = {}
 
     for c in param:
         subdf = df.loc[df['param']==c]
         dict_results[c] = subdf
-        subdf.to_csv('results/'+c+'.csv')
+        subdf.to_csv(data +c+'.csv')
 
     return dict_results
 
@@ -31,24 +32,46 @@ def normalize(data):
     return data
 
 #number of clusters + data
-def clustering(n_clusters, data, xstring, ystring):
-    #normalize?
-    #data_norm = normalize(data)
-    kmeans = KMeans(n_clusters, random_state=0)
-    kmeans.fit(data)
-    pred = kmeans.predict(data) + 1
+def clustering(n_clusters, data, xstring, ystring, zstring, wstring): #, vstring, kstring,gstring,mstring,hstring
+
+    #kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit_predict(data)
+
+    gm = GaussianMixture(n_components=n_clusters, random_state=0)
+    gm.fit(data)
+    #pred = kmeans.fit(data)
+    pred = gm.predict(data)
 
     x = []
     y = []
+    z = []
+    w = []
+    v = []
+    k = []
+    g = []
+    m = []
+    h = []
     for i in range(0, len(data)):
         x.append(data[i][0])
         y.append(data[i][1])
+        z.append(data[i][2])
+        w.append(data[i][3])
+        #v.append(data[i][4])
+       # k.append(data[i][5])
+       # g.append(data[i][6])
+       # m.append(data[i][7])
+       # h.append(data[i][8])
 
     df = pd.DataFrame()
     df[xstring] = x
     df[ystring] = y
+    df[zstring] = z
+    df[wstring] = w
+    #df[vstring] = v
+    #df[kstring] = k
+    #df[gstring] = g
+    #df[mstring] = m
+    #df[hstring]=h
     df['class'] = pred
 
     return df
 
-read_data('results/REF_111221_solved.txt')
