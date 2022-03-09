b436299bfe1ca085a8ce416841209c8eb835d524
Nandi Moksnes <nandi.moksnes@gmail.com>
2022-03-09 14:52:59 +0100

    added demand function

--- src/post_elec_GIS_functions.py
+++ src/post_elec_GIS_functions.py
@@ -120,75 +120,115 @@ def elec(demandcells):
     unelec.sum().reset_index()[['pointid']].to_csv(os.path.join(os.getcwd(), 'run/vision/un_elec.csv'))
     unelec.sum().reset_index()[['pointid']].to_csv(os.path.join(os.getcwd(), 'run/dryvision/un_elec.csv'))
 
-#This function is not used in the current version of the code
 def calculate_demand(settlements, demand):
-    demand_cell = pd.read_csv(settlements, index_col=[0])
-    demand_cell["pointid"] = demand_cell["pointid"].astype("category")
+    demand_cell = pd.read_csv(settlements, index_col=['ID'])
+    demand_GJ =  pd.read_csv(demand)
+    ref_demand = demand_GJ[demand_GJ['Scenario'].str.contains('ref')]
+    vision_demand = demand_GJ[demand_GJ['Scenario'].str.contains('Vision')]
 
-    totals_elec = pd.pivot_table(demand_cell, index=["elec"],
-                          aggfunc={'GDP_PPP': np.average, "Grid": np.min, "pop": np.sum, "pointid": np.sum}, margins=True,
-                          values=['GDP_PPP', "Grid", "pop", "pointid"])
-    total_gdp_elec = totals_elec['GDP_PPP'][1]
-    total_gdp_unelec = totals_elec['GDP_PPP'][0]
-    total_pop_elec = totals_elec['pop'][1]
-    total_pop_unelec = totals_elec['pop'][0]
+    demand_cols =  demand_cell[['elec', 'pointid', 'pop', 'GDP_PPP']]
+    #The case of unelectrified
+    un_elec = demand_cols[demand_cols['elec'] == 0]
+    unelec_pointid = un_elec.groupby(["pointid"]).sum()
+    unelec_pointid.sum().reset_index()
 
-    cell = pd.pivot_table(demand_cell, index=["pointid", "elec"], aggfunc= {'GDP_PPP' : np.average, "Grid": np.min, "pop": np.sum}, margins = True, values= ['GDP_PPP', "Grid","pop"])
+    sum_pop_unelec = sum(unelec_pointid['pop'])
+    un_elec_list= []
+    for i, row in unelec_pointid.iterrows():
+        row['un_elec_share'] = row['pop']/sum_pop_unelec
+        pointid = int(i)
+        row['Fuel'] = 'EL3_'+ str(pointid) + '_0'
+        startyear = 2016
+        while startyear <=2040:
+            unelec_ref = ref_demand[ref_demand['demand GJ'].str.contains('unelectrified')]
+            r = unelec_ref.index[0]
+            col = str(startyear)
+            row[col] = unelec_ref.loc[r,col]*row['un_elec_share']
+            startyear +=1
 
-    pd.concat(
-        [cell, cell.sum(level=[0, 1]).assign(iten_name='popsum').set_index('popsum', append=True)]).sort_index(
-        level=[0, 1, 2])
+        un_elec_list.append(row)
+        ind = row.index
 
-#
-    demand_emop = pd.read_csv('run/Demand/ref_demand.csv', index_col=0, header= 0)
-    demand_emop.columns = (demand_emop.columns).astype(int)
-    column_names = demand_emop.columns
-    index = range(len(cell.index))
-    demand = pd.DataFrame(columns=column_names, index= index)
-    demand.insert(0, 'Fuel', 0)
-#
-    locations = cell.index.get_level_values(0).unique()
-    j = 0
-#
-    for location in locations:
-        if location == 'All':
-            break
-        cell_id = int(location)
-        split = cell.xs(location)
-    #
-        for elec in split.index:
-            if elec ==1 and split['pop']!=0:
-                demand['Fuel'][j] ='EL3_%i_1' % (cell_id)
-                for i in demand_emop.columns:
-    #
-                    #This needs to be revised and also to sum over the electrified only. Also adding gdp is difficult here. Cannot be added like this at least.
-                    demand[i][j] = (0.5*split['pop'][elec] / total_pop_elec[0] + 0.5*split['GDP_PPP'][elec]/total_gdp_elec[0]) * demand_emop[i][0]
-                j +=1
-            elif elec==0 and split['pop']!=0:
-                demand['Fuel'][j] = 'EL3_%i_0' % (cell_id)
-                for i in demand_emop.columns:
+    ref_unelec = pd.DataFrame(un_elec_list, columns=ind)
 
-    #                     #This needs to be revised to sum over un-electrified.
-                    demand[i][j] = (split['pop'][elec]/total_pop_unelec[0])*demand_emop[i][1]
-                j +=1
-            else:
-                break
-    demand.index = demand['Fuel']
-    demand = demand.drop("Fuel", axis=1)
-    demand = demand.drop(0, axis=0)
-    demand.to_csv('run/demand.csv')
+    #The case of electrified
+    elec = demand_cols[demand_cols['elec'] == 1]
+    elec_pointid = elec.groupby(["pointid"]).sum()
+    elec_pointid.reset_index()
+
+    sum_pop_elec = sum(elec_pointid['pop'])
+    sum_gdp_elec = sum(elec_pointid['GDP_PPP'])
+
+    elec_list = []
+    for i, row in elec_pointid.iterrows():
+        row['elec_share'] = 0.5*row['pop']/sum_pop_elec+0.5*row['GDP_PPP']/sum_gdp_elec
+        pointid = int(i)
+        row['Fuel'] = 'EL3_'+ str(pointid) + '_1'
+        startyear = 2016
+        while startyear <=2040:
+            elec_ref = ref_demand[ref_demand['demand GJ'].str.contains('Electrified')]
+            r = elec_ref.index[0]
+            col = str(startyear)
+            row[col] = elec_ref.loc[r,col]*row['elec_share']
+            startyear +=1
+        elec_list.append(row)
+        ind = row.index
+
+    ref_elec = pd.DataFrame(elec_list, columns=ind)
+    ref = pd.concat(([ref_elec, ref_unelec]))
+    ref.index = ref['Fuel']
+    ref = ref.drop(columns =['elec', 'pop','GDP_PPP', 'elec_share', 'Fuel', 'un_elec_share'])
+    ref.to_csv('run/ref/ref_demand.csv')
+
+    #Vision scenario
+    sum_pop_unelec = sum(unelec_pointid['pop'])
+    un_elec_list= []
+    for i, row in unelec_pointid.iterrows():
+        row['un_elec_share'] = row['pop']/sum_pop_unelec
+        pointid = int(i)
+        row['Fuel'] = 'EL3_'+ str(pointid) + '_0'
+        startyear = 2016
+        while startyear <=2040:
+            unelec_vision = vision_demand[vision_demand['demand GJ'].str.contains('unelectrified')]
+            r = unelec_vision.index[0]
+            col = str(startyear)
+            row[col] = unelec_vision.loc[r,col]*row['un_elec_share']
+            startyear +=1
+
+        un_elec_list.append(row)
+        ind = row.index
+
+
+    vision_unelec = pd.DataFrame(un_elec_list, columns=ind)
+
+    #The case of electrified
+    elec = demand_cols[demand_cols['elec'] == 1]
+    elec_pointid = elec.groupby(["pointid"]).sum()
+    elec_pointid.reset_index()
+
+    sum_pop_elec = sum(elec_pointid['pop'])
+    sum_gdp_elec = sum(elec_pointid['GDP_PPP'])
+
+    elec_list = []
+    for i, row in elec_pointid.iterrows():
+        row['elec_share'] = 0.5*row['pop']/sum_pop_elec+0.5*row['GDP_PPP']/sum_gdp_elec
+        pointid = int(i)
+        row['Fuel'] = 'EL3_'+ str(pointid) + '_1'
+        startyear = 2016
+        while startyear <=2040:
+            vision_demand = vision_demand[vision_demand['demand GJ'].str.contains('Electrified')]
+            r = vision_demand.index[0]
+            col = str(startyear)
+            row[col] = vision_demand.loc[r,col]*row['elec_share']
+            startyear +=1
+        elec_list.append(row)
+        ind = row.index
+
+    vision_elec = pd.DataFrame(elec_list, columns=ind)
+    vision = pd.concat(([vision_elec, vision_unelec]))
+    vision.index = vision['Fuel']
+    vision = vision.drop(columns =['elec', 'pop','GDP_PPP', 'elec_share', 'Fuel', 'un_elec_share'])
+    vision.to_csv('run/vision/vision_demand.csv')
+    vision.to_csv('run/dryvision/vision_demand.csv')
 
     return ()
-
-if __name__ == "__main__":
-
-    shape = '../Projected_files/Final_Polygons_Kenya.shp'
-    gdp = '../Projected_files/masked_UMT37S_GDP_PPP_2015_WGS_84.tif'
-    elec_shp = '../Projected_files/elec.shp'
-    demandcells = os.path.join(os.getcwd(), 'run/Demand/demand_cells.csv')
-
-    #join(elec_shp, gdp, shape)
-    #elec(demandcells)
-    settlements =  'run/Demand/demand_cells.csv'
-    demand = 'run/Demand/demand.csv'
-    calculate_demand(settlements, demand)
